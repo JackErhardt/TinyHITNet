93f8fadcab94e51b2756828150c7b008a4615a9f
Jack M Erhardt <erharj@d1z.eecs.umich.edu>
2022-05-09 13:59:27 -0400

    Commented out slant from dataset loaders. May circle back to this in the future

--- dataset/kitti2015.py
+++ dataset/kitti2015.py
@@ -1,3 +1,7 @@
+# # Comment in when running as main
+# import sys
+# sys.path.append('/afs/eecs.umich.edu/vlsisp/users/erharj/TinyHITNet')
+
 import cv2
 import torch
 import numpy as np
@@ -32,9 +36,9 @@ class KITTI2015Dataset(Dataset):
         left_path = self.root / "image_2" / self.file_list[index]
         right_path = self.root / "image_3" / self.file_list[index]
         disp_path = self.root / "disp_occ_0" / self.file_list[index]
-        dxy_path = (
-            self.root / "slant_window" / self.file_list[index].with_suffix(".npy")
-        )
+        # dxy_path = (
+        #     self.root / "slant_window" / self.file_list[index].with_suffix(".npy")
+        # )
 
         data = {
             "left": np2torch(cv2.imread(str(left_path), cv2.IMREAD_COLOR), bgr=True),
@@ -43,7 +47,7 @@ class KITTI2015Dataset(Dataset):
                 cv2.imread(str(disp_path), cv2.IMREAD_UNCHANGED).astype(np.float32)
                 / 256
             ),
-            "dxy": np2torch(np.load(dxy_path), t=False),
+            # "dxy": np2torch(np.load(dxy_path), t=False),
         }
         if self.crop_size is not None:
             data = crop_and_pad(data, self.crop_size, self.training)
@@ -54,7 +58,8 @@ class KITTI2015Dataset(Dataset):
 
 if __name__ == "__main__":
     import torchvision
-    from colormap import apply_colormap, dxy_colormap
+    # from colormap import apply_colormap, dxy_colormap
+    from colormap import apply_colormap
 
     dataset = KITTI2015Dataset(
         "lists/kitti2015_train.list",
@@ -67,7 +72,8 @@ if __name__ == "__main__":
         disp = torch.clip(disp / 192 * 255, 0, 255).long()
         disp = apply_colormap(disp)
 
-        dxy = data["dxy"]
-        dxy = dxy_colormap(dxy)
-        output = torch.cat((data["left"], data["right"], disp, dxy), dim=0)
+        # dxy = data["dxy"]
+        # dxy = dxy_colormap(dxy)
+        # output = torch.cat((data["left"], data["right"], disp, dxy), dim=0)
+        output = torch.cat((data["left"], data["right"], disp), dim=0)
         torchvision.utils.save_image(output, "{:06d}.png".format(ids), nrow=1)
